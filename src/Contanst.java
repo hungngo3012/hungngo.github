37e891e9e7a32333498aa1407f6ba502e4d1d8af
hungngo3012 <hungngo189@gmail.com>
2020-11-25 23:14:20 +0700

    first commit

---
+++ src/Contanst.java
@@ -0,0 +1,9 @@
+package com.company;
+public class Contanst {
+    public static final String  API = "https://project-facebook-clone.herokuapp.com/it4788/user";
+    public static final String signUpAPI = API + "/signup";
+    public static final String change_passAPI = API + "/change_password";
+    public static final String set_blockAPI = API + "/set_block";
+    public static final String search_API = API + "/search";
+    public static final String log_inAPI = API + "/log_in";
+}
---
+++ src/ResponseLogin.java
@@ -0,0 +1,6 @@
+package com.company;
+public class ResponseLogin {
+    String code;
+    String message;
+    String token;
+}
